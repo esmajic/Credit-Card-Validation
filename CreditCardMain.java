0d4be2dab4b02f94855c1d7258601586644fc890
unknown <esmajic@yahoo.com>
2019-05-24 16:15:15 +0200

    works on main method code

--- CreditCardMain.java
+++ CreditCardMain.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class CreditCardMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+			
+		System.out.println("\n========================================");
+		System.out.println("      Credit Card Number Verification");
+		System.out.println("========================================");
+		System.out.println("\n==========================================");
+		System.out.println("Enter 1 for Visa Card number");
+		System.out.println("Enter 2 for Master Card number");
+		System.out.println("Enter 3 for American Express Card number");
+		System.out.println("Enter 4 for Discover Card number");
+		System.out.println("Enter 0 for Exit");
+		
+		long number = input.nextLong();
+		
 
 	}
 
