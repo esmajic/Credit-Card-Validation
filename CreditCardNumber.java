74bfe998607ab2585aa344c7e7336753398e0f9a
unknown <esmajic@yahoo.com>
2019-05-24 16:01:50 +0200

    created names for methods and finalized CreditCardNumber class

--- CreditCardNumber.java
+++ CreditCardNumber.java
@@ -1,4 +1,23 @@
 
 public class CreditCardNumber {
 
+	private long creditCardNumber;
+
+	public CreditCardNumber(long creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	public long getCreditCardNumber() {
+		return creditCardNumber;
+	}
+
+	public void setCreditCardNumber(long creditCardNumber) {
+		this.creditCardNumber = creditCardNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "CreditCardNumber [creditCardNumber=" + creditCardNumber + "]";
+	}
+
 }
--- CreditCardVerification.java
+++ CreditCardVerification.java
@@ -1,4 +1,28 @@
 
 public class CreditCardVerification {
 
+	
+	public static boolean isValid(long number) {
+		
+	}
+	
+	public static int sumOfDoubleEvePlace(long number) {
+		
+	}
+	
+	public static int getDigit(int number) {
+		
+	}
+	
+	public static int sumOfOddPlace(long number) {
+		
+	}
+	
+	public static boolean prefixMatched(long number, int d) {
+		
+	}
+	
+	public static int getSize(long d) {
+	
+	}
 }
